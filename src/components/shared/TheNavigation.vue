5a0199994a41214e9b890a035cc6940c51d13b72
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-15 14:41:39 +0100

    wip

--- src/components/shared/TheNavigation.vue
+++ src/components/shared/TheNavigation.vue
@@ -3,8 +3,8 @@
         <router-link to="/">Books</router-link>
         <div>
             <ul>
-                <li><router-link path="/">Hjem</router-link></li>
-                <li><router-link path="/profile"></router-link></li>
+                <li><router-link to="/">Hjem</router-link></li>
+                <li><router-link to="/profile"></router-link></li>
             </ul>
         </div>
     </nav>
---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>PROFIL</h1>
+</template>
