3f8e919638a8dc3be942c8728abd5454e3b13ba4
satanwa <sairanw@gmail.com>
2022-02-15 15:39:02 +0100

    oppdatering

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,18 @@
 <div>
   <router-view></router-view>
 </div>
-
 </template>
 
 <script>
 import TheNavigation from "./components/shared/TheNavigation.vue"
 
+
+
 export default {
   name: 'App', 
   components: {
     TheNavigation
+    
   }
 }
 
--- src/components/BooksItem.vue
+++ src/components/BooksItem.vue
@@ -1,3 +1,29 @@
 <template>
-    
+    <input type="text" v-model="search" placeholder="søk etter bøker">
+    <h2 v-for="name in filteredBooks" :key="name">{{ name }}</h2>
 </template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            books: [
+                'Call me by your name',
+                'Dune',
+                'Eleanor and Park',
+                'Harry Potter',
+            ],
+            search:""
+        }
+    },
+
+    computed: {
+        filteredBooks: function() {
+            return this.books.filter ((books) => {
+                return books.match(this.search);
+            });
+        }
+    }
+}
+</script>
