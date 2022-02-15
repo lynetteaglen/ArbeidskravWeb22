a3c56d358f7b54d39e54049d178c22201f2f7326
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-15 14:39:59 +0100

    wip

---
+++ src/components/shared/TheNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+    <nav>
+        <router-link to="/">Books</router-link>
+        <div>
+            <ul>
+                <li><router-link path="/">Hjem</router-link></li>
+                <li><router-link path="/profile"></router-link></li>
+            </ul>
+        </div>
+    </nav>
+</template>
