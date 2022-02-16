3b62442227681cdf3122eab9f43a5bdc74d655dd
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-16 19:36:19 +0100

    wip item

--- src/components/books/BooksItem.vue
+++ src/components/books/BooksItem.vue
@@ -1,9 +1,19 @@
 <template>
 
-<article>
-    <h3> {{ name }}</h3>
-</article>
-    
+  <article class="col-lg-4 col-sm-6 col-xs-12">
+
+  <div class="card mt-5" style="width: 18rem;">
+  <img :src="getBookImg()" class="card-img-top" alt="image of book">  
+  <div class="card-body">
+    <h5 class="card-title">{{ name }}</h5>
+    <p class="card-text">Genre: {{ genre }}</p>
+    <p class="card-text">Author: {{ author }}</p>
+    <a href="#" class="btn btn-primary">Add to reading list</a>
+  </div>
+</div>
+
+  </article>
+
 </template>
 
 <script>
@@ -12,6 +22,7 @@ export default({
 
     props: {
         name : String,
+        images : String, 
         genre: String, 
         author: String
     }
