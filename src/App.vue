8ddc5012808f6fb30ffde81b207a91429222b8f5
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-16 20:33:39 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
 <div class="container">
 <div>
   <router-view></router-view>
-
 </div>
 </div>
-
 
 </template>
 
 <script>
 import TheNavigation from "./components/shared/TheNavigation.vue"
+// import BooksItem from "./components/books/BooksItem.vue"
 
 export default {
   name: 'App', 
   components: {
-    TheNavigation
+    TheNavigation,
   }
 }
 
 </script>
+
