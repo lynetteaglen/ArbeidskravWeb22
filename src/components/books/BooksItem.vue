c4af0b5594b7df9b51ae70e99aabee7dfb7f4b44
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-16 19:20:08 +0100

    added images

--- src/components/books/BooksItem.vue
+++ src/components/books/BooksItem.vue
@@ -1,3 +1,20 @@
 <template>
+
+<article>
+    <h3> {{ name }}</h3>
+</article>
     
 </template>
+
+<script>
+
+export default({
+
+    props: {
+        name : String,
+        genre: String, 
+        author: String
+    }
+
+})
+</script>
