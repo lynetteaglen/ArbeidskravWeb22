397db60e070e29dc362702ae2f716f15e51d293f
satanwa <sairanw@gmail.com>
2022-02-16 20:55:29 +0100

    wip DRITT

--- src/components/books/BookList.vue
+++ src/components/books/BookList.vue
@@ -1,4 +1,27 @@
 <template>
-    
+    <h2> Books</h2>
+
+    <section>
+        <books-item v-for="i in booksArray" :key="i"
+        :name="booksArray.name"
+        :genre="booksArray.genre"
+        :author="booksArray.author"
+        ></books-item>
+    </section>
 </template>
+
+
+<script>
+import BooksItem from './BooksItem.vue'
+
+export default {
+    name: 'App',
+    setup(){},
+  components: { 
+      BooksItem 
+      }
+
+}
+
+</script>
 
