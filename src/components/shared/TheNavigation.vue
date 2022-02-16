462d5f00a4abf898007d57c9ad46220116c132be
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-16 17:20:18 +0100

    wip

--- src/components/shared/TheNavigation.vue
+++ src/components/shared/TheNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <router-link to="/">ViSomElskerBøker</router-link>
         <div>
             <ul>
@@ -8,4 +8,26 @@
             </ul>
         </div>
     </nav>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand"><router-link to="/">Bookies</router-link></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page"><router-link to="/">Hjem</router-link></a>
+        <a class="nav-link"><router-link to="/profile">Profil</router-link></a>
+      </div>
+    </div>
+  </div>
+</nav>
+
 </template>
+
+<style>
+
+
+
+</style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,3 +1,37 @@
 <template>
     <h1>PROFIL</h1>
+
+    <h2>Velkommen, Timothée</h2>
+
+    <section>
+        <h2>Dine favoritter</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+
+
+    <section>
+        <h2>Har lest</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Vil lese</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+
+
 </template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
