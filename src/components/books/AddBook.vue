fbf44b95526199909f868fd0075accb8fc7cb124
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-17 21:32:52 +0100

    wip

--- src/components/books/AddBook.vue
+++ src/components/books/AddBook.vue
@@ -4,9 +4,15 @@
     <h2>Legg til en bok i leselista</h2>
     <div class="col-lg-12 col-sm-6 col-xs-12">
         <form>
-        <input type="text" v-model="searchBooks" placeholder="Søk etter en bok"/>
-        <button @click="books">Søk</button>
+        <input type="text" v-model="listInput" placeholder="Søk etter navn på bok"/>
+        <button @click="addBook">Legg til</button>
         </form>
+
+        <h2>Din leseliste</h2>
+        <ul>
+            <!-- <li v-for="(book, index) in newBook" :key="index">{{ book }}</li> -->
+            <li v-for="(book , index) in titleArray" :key="index">{{ book }}</li>
+        </ul>
 
     </div>
 </section>
@@ -20,33 +26,38 @@ import { ref } from 'vue'
 export default({
     setup(){
 
-const books = ref([
-      `Call me by your name`, 
-      `Find me`, 
-      `The Untethered Soul`,
-      `The Power of Now`
-])
+        let listInput = ref("")
 
-const newBook = ref(``) 
 
-const removeBook = (book) => {
-    books.value = books.value.filter((item) => {
-        return book != item
-    }) 
-}
+        const titleArray = ref([
+            `Call me by your name`, 
+            `Find me`, 
+            `The Untethered Soul`,
+            `The Power of Now`
+        ])
 
+// change to newBook? 
 const addBook = () => {
-    if(!books.value.includes(newBook.value)){
-        books.value.push(newBook.value)
-    }
-    newBook.value = ``
+    let userInput = listInput.value
+    userInput.value.push( titleArray ); 
+    // titleArray.value.push(userInput)
+    // userInput.push(booksArray.value)
+    // titleArray.value.add(userInput)
+    console.log(userInput) 
 }
+
+
+
+// const removeBook = (book) => {
+//     booksArray.value = booksArray.value.filter((item) => {
+//         return book != item
+//     }) 
+// }
+
 
 return {
-    books, 
-    newBook, 
-    addBook, 
-    removeBook
+    titleArray, 
+    addBook
 }
 
     }
