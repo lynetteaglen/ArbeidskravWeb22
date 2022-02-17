c49a6c1a4de470548445fed02d0cf5c15babc503
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-17 13:51:14 +0100

    fixed file

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -2,19 +2,23 @@
     
     <section>
         <book-list></book-list>
+        <add-book><add-book>
     </section>
 
 </template>
 
 <script>
+import AddBook from '../components/books/AddBook.vue'
 import BookList from '../components/books/BookList.vue'
 
 export default {
     name: 'App', 
     components: {
         BookList,
+        AddBook
 
     }
 
 }
+
 </script>
