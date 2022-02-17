fb8bf19bb155fb39cb93e96b8158b7e060de2f7e
Lynette Aglen <lynette.aglen@gmail.com>
2022-02-17 12:43:53 +0100

    fixed images

--- src/components/books/BooksItem.vue
+++ src/components/books/BooksItem.vue
@@ -1,49 +1,45 @@
 <template>
 
   <article class="col-lg-4 col-sm-6 col-xs-12">
-
   <div class="card mt-5" style="width: 18rem;">
-  <!-- <img :src="getImage()" class="card-img-top" alt="image of book">  -->
+  <img :src="getImage()" class="card-img-top" alt="image of book"> 
   <div class="card-body">
     <h5 class="card-title">{{ name }}</h5>
     <p class="card-text">Genre: {{ genre }}</p>
     <p class="card-text">Author: {{ author }}</p>
     <a href="#" class="btn btn-primary">Add to reading list</a>
   </div>
-</div>
-
+  </div>
   </article>
 
 </template>
 
 <script>
-// import { ref } from 'vue'
+// import booksService from "../../service/booksService";
 
 export default({
 
     props: {
         name : String,
-        images : String, 
+        image : String, 
         genre: String, 
-        author: String
+        author: String,
     }, 
 
+    setup(props){
 
-    // setup(props){
+        const getImage = () => {
+            let image
+            try{ 
+                image = require(`@/assets/images/${ props.image }` );
+            }catch{
+                image = ''
+            }
+            return image
+        }
 
-    //     const getImage = () => {
-    //         let image;
-    //         try{ 
-    //             image = require( `@/assets/images/${ props.image }` );
-    //         }catch{
-    //             image = require( `` );
-    //         }
-    //         return image;
-    //     }
-
-    //     return { getImage }
-
-    // }
+        return { getImage }
+    }, 
 
 })
 
